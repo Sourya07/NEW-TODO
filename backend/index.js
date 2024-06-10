4fe0d3d4d1149f9c3522a3759bbc0cf5df9f88f2
Sourya07 <singhsourya137@gmail.com>
2024-06-11 01:20:28 +0530

    modifiying index.js routes

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,35 @@
 const express =require("express")
 const app = express()
-
+const { createTodo, updateTodo } = require("./types")
 app.use(express.json());
 
 
 
 app.post("todo",function(req,res){
 
+const createPayload = req.body
+ const parsePayload =createTodo.safeParse(createPayload)
+
+
+ if(!parsePayload.success){
+    res.status(400).json({error:"Invalid input"})
+}
+return ;
 })
+
+
+
 
 app.get("/todos",function(req,res){
     
 })
 app.put("/completed",function(req,res){
     
+const updatePayload =req.body
+const parsePayload = updateTodo.safeParse(updatePayload)
+if(!parsePayload.success){
+    res.status(400).json({error:"Invalid input"})
+}
+return ;
 })
+
