e002f79649c97c8672a2a35f66d49c127f8a6ce9
Sourya07 <singhsourya137@gmail.com>
2024-06-11 18:09:48 +0530

    adding index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express =require("express")
 const app = express()
-const { createTodo, updateTodo } = require("./types")
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 app.use(express.json());
 
 
 
-app.post("todo",function(req,res){
+app.post("todo",  async function(req,res){
 
 const createPayload = req.body
  const parsePayload =createTodo.safeParse(createPayload)
@@ -13,23 +14,40 @@ const createPayload = req.body
 
  if(!parsePayload.success){
     res.status(400).json({error:"Invalid input"})
+    return ;
 }
-return ;
+await todo.create({
+title:createPayload.title,
+description:createPayload.description,
+completed: false
+})
+res.json({
+    msg:"todo created"
+})
+
 })
 
 
 
 
-app.get("/todos",function(req,res){
+app.get("/todos",async function(req,res){
+ 
+    const todos = await todo.find({})
+    res.json({
+        todos
+    })
+
     
 })
+
 app.put("/completed",function(req,res){
     
 const updatePayload =req.body
 const parsePayload = updateTodo.safeParse(updatePayload)
 if(!parsePayload.success){
     res.status(400).json({error:"Invalid input"})
+    return ;
 }
-return ;
+
 })
 
